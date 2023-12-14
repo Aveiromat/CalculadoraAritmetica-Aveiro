e5c485b34d1bc0675d554fd3c572a8c283191968
Matheus Aveiro <Matheusaveirofagundes@gmail.com>
2023-12-14 18:14:50 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { reactive } from 'vue';
+import Cabecalho from './components/Cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
+
+const estado = reactive({
+  filtro: 'todas',
+  tarefaTemp: '',
+  tarefa: [
+    {
+      titulo: 'Estudar ES6',
+      finalizada: false,
+    },
+    {
+      titulo: 'Estudar SASS',
+      finalizada: false,
+    },
+    {
+      titulo: 'Ir para a academia',
+      finalizada: true,
+    },
+  ]
+})
+
+const getTarefasPendentes = () => {
+  return estado.tarefa.filter(tarefa => !tarefa.finalizada)
+}
+
+const getTarefasFinalizadas = () => {
+  return estado.tarefa.filter(tarefa => tarefa.finalizada)
+}
+
+
+const getTarefasFiltradas = () => {
+  const { filtro } = estado
+
+  switch (filtro) {
+    case 'pendentes':
+      return getTarefasPendentes();
+    case 'finalizadas':
+      return getTarefasFinalizadas();
+    default:
+      return estado.tarefa;
+  }
+}
+
+const cadastraTarefa = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemp,
+    finalizada: false,
+  }
+  estado.tarefa.push(tarefaNova);
+  estado.tarefaTemp = '';
+}
+</script>
+
+<template>
+  <div class="container d-flex flex-column vh-100 justify-content-center col-6 offset-3">
+    <div>
+      <Cabecalho :tarefaPendentes="getTarefasPendentes().length" />
+      <Formulario :trocarFiltro="evento => estado.filtro = evento.target.value" :tarefaTemp="estado.tarefaTemp" :editaTarefaTemp="evento => estado.tarefaTemp = evento.target.value" :cadastraTarefa="cadastraTarefa" />
+    </div>
+  </div>
+</template>
---
+++ src/components/Cabecalho.vue
@@ -0,0 +1,11 @@
+<script setup>
+    const props = defineProps(['tarefaPendentes'])
+</script>
+
+<template>
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+        <h1>
+            O resultado é: {{ props.tarefaPendentes }}
+        </h1> 
+    </header>
+</template>
